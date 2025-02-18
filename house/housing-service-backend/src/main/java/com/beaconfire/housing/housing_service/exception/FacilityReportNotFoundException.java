348202e2bb9acbcef54f9830318a7a776034e306
Jince Shi <120279910+jshi98@users.noreply.github.com>
2025-02-17 19:55:02 -0500

    Delete house directory

--- house/housing-service-backend/src/main/java/com/beaconfire/housing/housing_service/exception/FacilityReportNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.beaconfire.housing.housing_service.exception;
-
-public class FacilityReportNotFoundException extends RuntimeException {
-    public FacilityReportNotFoundException(String message) {
-        super(message);
-    }
-}
--- house/housing-service-backend/src/main/java/com/beaconfire/housing/housing_service/exception/LandlordNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.beaconfire.housing.housing_service.exception;
-
-public class LandlordNotFoundException extends RuntimeException {
-    public LandlordNotFoundException(String message) {
-        super(message);
-    }
-}
